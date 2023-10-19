b380a70637eaa43fb592ad247d58264998f2d933
AureliusAtilius <j.j.h.lindner@gmail.com>
2023-10-19 13:30:56 -0700

    Fix if conditional spacing.

--- saskatoon.sh
+++ saskatoon.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 ips=$(cat /home/admin/access.log | awk '{print $1}')
-maximum=$0
+maximum=0
 maxIP=""
-check=$0
+check=0
 for ip in $ips
-    do
-    echo $ip
-    check=$(grep $ip /home/admin/access.log | wc -l)
-    if [$check -gt $maximum]
+do
+    
+    check=$(grep -c $ip /home/admin/access.log )
+    if [[ $check -gt $maximum ]]
     then
         maximum=$check
         maxIP=$ip
     fi
-    done 
+done
 
 echo $maxIP > /home/admin/highestip.txt
 
